b78c6c46d2754e317f9afb34f6d6040159b8539c
Markpm-code <marlonseglos@yahoo.com>
2023-08-24 12:54:00 +0000

    update allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 
 X_FRAME_OPTIONS ='SAMEORIGIN'
 
-ALLOWED_HOSTS = ['codestar-blog1.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-blog1-8911d6384e81.herokuapp.com', 'localhost']
 
 
 # Application definition
